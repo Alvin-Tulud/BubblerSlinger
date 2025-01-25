b202ddcca7b3dea1fe214d801bc3f5078e515606
Alvin-Tulud <alvintulud@gmail.com>
2025-01-24 17:25:38 -0800

    statemachine for level state start

--- SlingingBubbles/Assets/Scripts/GameState/State.cs
+++ SlingingBubbles/Assets/Scripts/GameState/State.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class LevelStart : MonoBehaviour
+public class State : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
