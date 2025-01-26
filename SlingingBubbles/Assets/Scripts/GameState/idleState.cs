59cb58049690242a72edcb39b6a6108a0d8a51bf
Alvin-Tulud <alvintulud@gmail.com>
2025-01-25 17:37:17 -0800

    added player to state also tagged player

--- SlingingBubbles/Assets/Scripts/GameState/idleState.cs
+++ SlingingBubbles/Assets/Scripts/GameState/idleState.cs
@@ -2,14 +2,17 @@ using UnityEngine;
 
 public class idleState : State
 {
-    dieState die;
-    winState win;
+    private dieState die;
+    private winState win;
+    private BubbleMovement PlayerScript;
     //get player gameobject
 
     void Start()
     {
         die = GetComponent<dieState>();
         win = GetComponent<winState>();
+
+        PlayerScript = GameObject.FindWithTag("Player").GetComponent<BubbleMovement>();
     }
 
     public override State ChangeState()
@@ -19,6 +22,11 @@ public class idleState : State
 
     public override State RunCurrentState()
     {
+        if (PlayerScript.getIsDead())
+        {
+            return die;
+        }
+
         return this;
     }
 }
